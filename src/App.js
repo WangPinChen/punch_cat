b38fbf346e2cf352cca8b7dc9832586cb176e09b
WangRPing <r17369@gmail.com>
2023-11-13 17:29:33 +0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    // console.log(process.env.REACT_APP_API_URL, process.env.REACT_APP_API_PATH)
-
-    // console.log(`${process.env.REACT_APP_API_URL}/v2/api/${process.env.REACT_APP_API_PATH}/products/all`)
-
     (async () => {
       const res = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`)
       console.log(res)
@@ -15,6 +11,16 @@ function App() {
   }, [])
   return (
     <div className="App">
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './stylesheets/all.scss'
 import App from './App';
 import axios from 'axios';
 
