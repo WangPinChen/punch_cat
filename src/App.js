a8314fee1291f7fb475f423a615d457482fc8f1c
WangRPing <r17369@gmail.com>
2023-11-13 18:30:50 +0800

    add login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { Route, Routes } from 'react-router-dom'
+
+import Login from "./pages/Login";
 
 
 function App() {
-  useEffect(() => {
-    (async () => {
-      const res = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`)
-      console.log(res)
-    })()
-  }, [])
+  // useEffect(() => {
+  //   (async () => {
+  //     const res = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`)
+  //     console.log(res)
+  //   })()
+  // }, [])
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
-
-      <button type="button" class="btn btn-link">Link</button>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './stylesheets/all.scss'
-import App from './App';
+import { HashRouter } from 'react-router-dom'
 import axios from 'axios';
+
+import './stylesheets/all.scss'
+
+import App from './App';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
